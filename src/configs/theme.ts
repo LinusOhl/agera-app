376e33c40fb893f2eac94df514bc92259de30f6f
Linuss <tade3d@gmail.com>
2026-01-16 13:31:00 +0100

    Updated theme.

--- src/configs/theme.ts
+++ src/configs/theme.ts
@@ -1,5 +1,10 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  fontFamily: "Parkinsans",
+  fontFamily: "Parkinsans, sans-serif",
+  headings: {
+    fontFamily: "Parkinsans, sans-serif",
+  },
+  black: "#282B28",
+  white: "#F6F4F3",
 });
