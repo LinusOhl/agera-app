712e224a41ec0b4e6cee20d6644ca7f889f4da63
Linuss <tade3d@gmail.com>
2026-01-17 12:56:58 +0100

    Created a base login page.

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,4 +1,24 @@
-import { createFileRoute } from "@tanstack/react-router";
+import {
+  Alert,
+  Button,
+  Card,
+  Center,
+  Flex,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconArrowNarrowLeft } from "@tabler/icons-react";
+import {
+  createFileRoute,
+  Link,
+  linkOptions,
+  useNavigate,
+} from "@tanstack/react-router";
+import { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 
 export const Route = createFileRoute("/login")({
@@ -6,21 +26,93 @@ export const Route = createFileRoute("/login")({
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+  const theme = useMantineTheme();
   const { login } = useAuth();
 
-  return (
-    <div>
-      <h1>Welcome to Agera!</h1>
-      <p>Login page</p>
+  const indexLinkOptions = linkOptions({
+    to: "/",
+  });
 
-      <button
-        type="button"
-        onClick={async () => {
-          await login("johnny@doe.com", "johnny");
-        }}
-      >
-        Log in
-      </button>
-    </div>
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogin = async () => {
+    if (!email.trim()) {
+      return;
+    }
+
+    if (!password.trim()) {
+      return;
+    }
+
+    try {
+      await login(email, password);
+
+      navigate({ to: "/user" });
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+    } finally {
+      setEmail("");
+      setPassword("");
+    }
+  };
+
+  return (
+    <Center maw={"100%"} h={"100vh"}>
+      <Flex direction={"column"}>
+        <Title order={1} mb={"xl"} fz={96} c={"dark"}>
+          agera
+        </Title>
+
+        <Card padding={"lg"} radius={"md"} shadow="md" mb={"xl"}>
+          <Text fz={"lg"} fw={500} mb={"lg"} c={"dark"}>
+            Log in to manage your tasks
+          </Text>
+
+          <Stack gap={"md"}>
+            {error && (
+              <Alert variant="light" color="red">
+                {error}
+              </Alert>
+            )}
+
+            <TextInput
+              label={"Email"}
+              placeholder="john@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+
+            <PasswordInput
+              label="Password"
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+            />
+
+            <Button variant="outline" color="dark" onClick={handleLogin}>
+              Log in
+            </Button>
+          </Stack>
+        </Card>
+
+        <Button
+          variant="transparent"
+          color="dark"
+          size="compact-sm"
+          leftSection={<IconArrowNarrowLeft color={theme.colors.dark[6]} />}
+          style={{
+            alignSelf: "flex-start",
+          }}
+          component={Link}
+          {...indexLinkOptions}
+        >
+          Go back
+        </Button>
+      </Flex>
+    </Center>
   );
 }
