f59b2faa9293526a26c3505d7d941e7489a532af
Linuss <tade3d@gmail.com>
2026-01-17 12:13:44 +0100

    Created a base index page.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,43 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Box, Button, Card, Center, Flex, Text, Title } from "@mantine/core";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: Home,
+  component: RouteComponent,
 });
 
-function Home() {
+function RouteComponent() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Welcome to Agera!</h1>
-      <p>Homepage</p>
-    </div>
+    <Box>
+      <Center maw={"100%"} h={"100vh"}>
+        <Flex direction={"column"}>
+          <Flex direction={"column"}>
+            <Text fz={"lg"} fw={500}>
+              Welcome to
+            </Text>
+            <Title order={1} mb={"xl"} fz={96}>
+              agera
+            </Title>
+          </Flex>
+
+          <Card padding={"lg"} radius={"md"} shadow="md">
+            <Flex direction={"column"} gap={"md"}>
+              <Button
+                variant="outline"
+                color="dark"
+                onClick={() => navigate({ to: "/login" })}
+              >
+                Log in
+              </Button>
+
+              <Button color="dark" onClick={() => navigate({ to: "/signup" })}>
+                Sign up
+              </Button>
+            </Flex>
+          </Card>
+        </Flex>
+      </Center>
+    </Box>
   );
 }
